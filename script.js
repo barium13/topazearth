2dd76f7e210005386f32cd3b89e8fe13b9b3ff5f
barium <41872401+barium13@users.noreply.github.com>
2025-10-26 14:06:04 +0000

    Mobile use compatable

--- script.js
+++ script.js
@@ -20,13 +20,3 @@ serverIp.addEventListener('click', () => {
       }, 1200);
     });
 });
-
-<script>
-const hamburger = document.getElementById('hamburger');
-const navLinks = document.getElementById('nav-links');
-
-hamburger.addEventListener('click', () => {
-  hamburger.classList.toggle('active');
-  navLinks.classList.toggle('show');
-});
-</script>
